677c8e6696c9d82b15d47d193a8eb8282a1679db
Lakshita-1502 <lakshitac1502@gmail.com>
2025-12-11 20:02:34 +0530

    Added update method for tasks and value points calculations

--- src/main.py
+++ src/main.py
@@ -1,36 +1,65 @@
 taskDict={}
-count=1
-more="y"
-ans="y"
+totalTasks=1
+moreTask="yes"
+wantToContinue="yes"
 
-while (ans=="y"):
-    more="y"
-    print("Personal Productivity Manager\n")
+#tasks CRUD operations
+while (wantToContinue.lower()=="yes"):
+    moreTask="yes"
+    print("\nPersonal Productivity Manager\n")
     print("""Following are the choices offered by us:- 
         1. Add Task
         2. Update Task
         3. Delete Task
-        4. Display all the Task""")
+        4. Display Task""")
     choice=int(input("\nEnter the choice number:- "))
 
-    while (more=="y"):
+    while (moreTask.lower()=="yes"):
         if (choice==1):
-            taskId=count
-            taskName=input("Enter you task:- ")
+            taskSubDict={}
+            taskId=totalTasks
+            taskName=input("\nEnter your task's description:- ")
             valuePoints=int(input(f"Enter value points (1-5) to Task {taskId}:- "))
-            pendingCompleted=0
-            taskArray=[taskName, valuePoints, pendingCompleted]
-            taskDict[taskId]=taskArray
-            count+=1
-            more=input("Want to add more tasks? (y/n):- ")
+            pendingCompleted="no"
+            taskSubDict["taskName"]=taskName
+            taskSubDict["valuePoints"]=valuePoints
+            taskSubDict["pendingCompleted"]=pendingCompleted
+            taskDict[taskId]=taskSubDict
+            totalTasks+=1
+            moreTask=input("Want to add more tasks:- ")
+
+        elif (choice==2):
+            taskId=int(input("\nEnter task ID you completed:- "))
+            taskDict[taskId]["pendingCompleted"]="yes"
+            moreTask=input("Any more task completed:- ")
+
         elif (choice==3):
-            taskId=int(input("Enter task ID you want to delete:- "))
+            taskId=int(input("\nEnter task ID you want to delete:- "))
             del taskDict[taskId]
-            more=input("Want to delete more tasks? (y/n):- ")
+            moreTask=input("Want to delete more tasks:- ")
+
         elif (choice==4):
             for key, value in taskDict.items():
-                print(f"Task ID:- {key}\nTask Description:- {value[0]}\nValue Points:- {value[1]}\nIs Completed:- {value[2]}")
-            more="n"
-            ans=input("Want to contnue? (y/n):- ")
+                print(f"\nTask ID:- {key}\n"+
+                      f"Task Description:- {value["taskName"]}\n"
+                      f"Value Points:- {value["valuePoints"]}\n"
+                      f"Completed:- {value["pendingCompleted"]}")
+            moreTask="no"
+            wantToContinue=input("\nWant to continue:- ")
 
+        else:
+            print("Select a proper choice number")
+
+#value point calculations
+totalValuePoints=0
+totalCompletedValuePoints=0
+for value in taskDict.values():
+    totalValuePoints+=value["valuePoints"]
+    if value["pendingCompleted"]=="yes":
+        totalCompletedValuePoints+=value["valuePoints"]
+
+print(f"Total Value Points for the day:- {totalValuePoints}")
+print(f"Total Value Points of completed tasks for the day:- {totalCompletedValuePoints}")
+productivity=round((totalCompletedValuePoints/totalValuePoints)*100, 2)
+print(f"Productivity percentage:- {productivity}%")
 
