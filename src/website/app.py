46bbb1fa6fa13d216162de3ec786b2a87cb9a1ee
Lakshita-1502 <lakshitac1502@gmail.com>
2025-12-16 01:27:53 +0530

    Displayed tasks and added value points

--- src/website/app.py
+++ src/website/app.py
@@ -1,13 +1,19 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, request
 
 app=Flask(__name__)
+tasks={}
+taskId=1
 
-@app.route('/')
-def dashboard():
-    return render_template('dashboard.html')
+@app.route('/', methods=['POST', 'GET'])
+def taskForm():   
+    global taskId
+    if request.method=="POST":
+        taskDesc=request.form.get("taskName")
+        taskValue=request.form.get("valuePoint")
+        if taskDesc:
+            tasks[taskId]={"description": taskDesc, "valuePoints": taskValue}
+            taskId+=1
+    return render_template('taskForm.html',tasks=tasks)
 
-@app.route('/add-task', methods=['POST'])
-def taskForm():     
-    return render_template('taskForm.html')
 if __name__=="__main__":
     app.run(debug=True)
